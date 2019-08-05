0a020014ac63bd648b14a92c4dea1fbfc2b6d4d2
赤嶺昂樹 <akaminekoki@akaminenoborujunoMacBook-Air.local>
2019-08-05 10:43:46 +0900

    mypage-profile

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,2 +1,6 @@
 class UsersController < ApplicationController
+
+  def mypage_profile
+    
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,4 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  get 'users/mypage_profile' => 'users#mypage_profile'
 end
