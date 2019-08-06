df45bb17cdae76b5bb1292e67e27f0f52373aaa4
yuichi.kitagawa <nagi09041192@yahoo.co.jp>
2019-08-06 15:07:12 +0900

    during-creating-product-detail

--- config/routes.rb
+++ config/routes.rb
@@ -2,9 +2,11 @@ Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 
   get 'products' => 'products#toppage'
+  get "products/detail" => "products#products_detail"
   get 'users/mypage_profile' => 'users#mypage_profile'
   get "/users/signup" => "users#signup"
   get "/users/signup/registration" => "users#registration"
   get "/users/signup/sms_confirmation" => "users#sms_confirmation"
   get "/users/signup/sms_authentication" => "users#sms_authentication" 
+
 end
