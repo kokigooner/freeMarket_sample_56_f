826c2d5a54d7c82dec573c28d41156fcaccd387e
katsu18 <52535077+katsu18@users.noreply.github.com>
2019-08-10 00:16:38 +0900

    test_model追加

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -8,7 +8,9 @@ class ProductsController < ApplicationController
     test_model2 = {name:"コーギー2",price:"400",like:"20"}
     test_model3 = {name:"コーギー3",price:"300",like:"30"}
     test_model4 = {name:"コーギー4",price:"400",like:"40"}
-    @test_model = hash.push(test_model1,test_model2,test_model3,test_model4)
+    test_model5 = {name:"コーギー3",price:"300",like:"30"}
+    test_model6 = {name:"コーギー4",price:"400",like:"40"}
+    @test_model = hash.push(test_model1,test_model2,test_model3,test_model4,test_model5,test_model6)
   end
 
   def confirm
