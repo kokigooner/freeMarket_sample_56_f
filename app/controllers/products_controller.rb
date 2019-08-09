f2d15cb214a4f7012c413a006362f13e4965537e
katsu18 <52535077+katsu18@users.noreply.github.com>
2019-08-09 21:51:50 +0900

    each文追加

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -2,7 +2,13 @@ class ProductsController < ApplicationController
   def toppage
   end
 
-  def detail
+  def products_detail
+    hash        = []
+    test_model1 = {name:"コーギー1",price:"400",like:"10"}
+    test_model2 = {name:"コーギー2",price:"400",like:"20"}
+    test_model3 = {name:"コーギー3",price:"300",like:"30"}
+    test_model4 = {name:"コーギー4",price:"400",like:"40"}
+    @test_model = hash.push(test_model1,test_model2,test_model3,test_model4)
   end
 
   def confirm
