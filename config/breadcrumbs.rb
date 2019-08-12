0f2b8c30bde2ae45a9a8d50848d28c6f5562a458
katsu18 <52535077+katsu18@users.noreply.github.com>
2019-08-12 21:31:57 +0900

    パンくず機能追加（マイページ）

--- config/breadcrumbs.rb
+++ config/breadcrumbs.rb
@@ -3,5 +3,23 @@ crumb :root do
 end
 
 crumb :user do |user|
-  link 'マイページ'
+  link 'マイページ',"/users/mypage/#{current_user.id}"
 end
+
+
+crumb :mypage_identification do |user|
+  link "本人情報の登録 ", "/mypage/identification"
+  parent :user, user
+end
+
+
+crumb :mypage_card do |user|
+  link "支払い方法", "/mypage/card"
+  parent :user, user
+end
+
+crumb :profile do |user|
+  link "プロフィール", "/users/mypage/profile"
+  parent :user, user
+end
+
