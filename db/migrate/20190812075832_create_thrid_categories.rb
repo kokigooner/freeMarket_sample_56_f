bddf2205f26e3e37f2c234ed780266f23fcfaa2f
katsu18 <52535077+katsu18@users.noreply.github.com>
2019-08-13 09:11:38 +0900

    sider

--- db/migrate/20190812075832_create_thrid_categories.rb
+++ db/migrate/20190812075832_create_thrid_categories.rb
@@ -1,6 +1,6 @@
 class CreateThridCategories < ActiveRecord::Migration[5.0]
   def change
-    create_table :thrid_categories do |t|
+    create_table :third_categories do |t|
       t.string :third_category,null: false
       t.references :second_category, foreign_key: true
     end
--- spec/factories/third_categories.rb
+++ spec/factories/third_categories.rb
@@ -1,5 +1,5 @@
 FactoryBot.define do
-  factory :thrid_category do
+  factory :third_category do
     third_category { "MyString" }
     second_category_id { "" }
   end
