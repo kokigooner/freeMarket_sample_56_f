bafdd499c5c6425972523785054f374bb480fd26
kaseken <k.kaseda0925@gmail.com>
2019-08-13 12:48:15 +0900

    fix_errors_found_by_sider

--- config/routes.rb
+++ config/routes.rb
@@ -2,27 +2,28 @@ Rails.application.routes.draw do
   devise_for :users, controllers: { omniauth_callbacks: 'users/omniauth_callbacks' }
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   root to: "products#toppage"
-  get '/products' => 'products#toppage'
-  get "/products/:id" => "products#products_detail" 
-  get "/products/confirm" => "products#confirm"
-  get "/products/sell" => "products#sell"
-  get '/users/mypage/profile' => 'users#profile'
-  get '/users/mypage/card' => "users#card"
-  get '/users/mypage/card/create' => "users#card_create"
-  get "/users/mypage" => "users#mypage"
-  get "/mypage/identification" => "users#identification" 
-  get "/mypage/logout" => "users#logout"
-  
-  get "/users/signup" => "users#signup"
-  post "/users/signup" => "users#create"
-  get "/users/signup/registration" => "users#registration"
-  post "/users/signup/confirmation" => "users#confirmation"
-  get "/users/signup/authentication" => "users#authentication"
-  get "/users/signup/address" => "users#address"
-  post "/users/signup/payment" => "users#payment"
-  get "/users/signup/complete" => "users#complete"
+  get '/products', to: 'products#toppage'
+  get "/products/confirm", to: "products#confirm"
+  get "/products/sell", to: "products#sell"
+  get "/products/:id", to: "products#products_detail" 
 
-  get "/users/sell" => "users#sell"
-  get "/users/login" => "users#login"
-  get '/users/sign_out' => 'devise/sessions#destroy'
+  get '/users/mypage/profile', to: 'users#profile'
+  get '/users/mypage/card', to: "users#card"
+  get '/users/mypage/card/create', to: "users#card_create"
+  get "/users/mypage", to: "users#mypage"
+  
+  get "/mypage/identification", to: "users#identification" 
+  get "/mypage/logout", to: "users#logout"
+  
+  post "/users/signup", to: "users#create"
+  get "/users/signup/registration", to: "users#registration"
+  post "/users/signup/confirmation", to: "users#confirmation"
+  get "/users/signup/authentication", to: "users#authentication"
+  get "/users/signup/address", to: "users#address"
+  post "/users/signup/payment", to: "users#payment"
+  get "/users/signup/complete", to: "users#complete"
+
+  get "/users/sell", to: "users#sell"
+  get "/users/login", to: "users#login"
+  get '/users/sign_out', to: 'devise/sessions#destroy'
 end
