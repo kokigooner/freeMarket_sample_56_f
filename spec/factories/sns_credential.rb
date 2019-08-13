b4cb4c57a757899f4d6375c751122fe34c58ebfb
kaseken <k.kaseda0925@gmail.com>
2019-08-13 15:31:39 +0900

    fix_errors

--- spec/factories/sns_credential.rb
+++
@@ -1,9 +0,0 @@
-FactoryBot.define do
-
-  factory :user do
-    provider                  {"facebook"}
-    uid                   {"000000000000000"}
-    user_id                 {"1"}
-  end
-
-end
