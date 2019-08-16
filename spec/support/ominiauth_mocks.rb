46610a942beaabd489e7302893e92e20f03ea781
赤嶺昂樹 <akaminekoki@akaminenoborujunoMacBook-Air.local>
2019-08-16 21:40:02 +0900

    sider1

--- spec/support/ominiauth_mocks.rb
+++ spec/support/ominiauth_mocks.rb
@@ -1,23 +1,23 @@
-module OmniauthMocks
-  def facebook_mock
-    Omniauth.config.mock_auth[:facebook] = OmniAuth: :AuthHash.new({
-      "provider" = "facebook",
-      "uid" = "123456",
-      "name" = "メルカリ　太朗"
-      "email" = "aaa@gmail.com"
-    })
-  end
+# module OmniauthMocks
+#   def facebook_mock
+#     Omniauth.config.mock_auth[:facebook] = OmniAuth: :AuthHash.new({
+#       "provider" = "facebook",
+#       "uid" = "123456",
+#       "name" = "メルカリ　太朗"
+#       "email" = "aaa@gmail.com"
+#     })
+#   end
 
-  def google_mock
-    Omniauth.config.mock_auth[:google]
-      "provider" = "google",
-      "uid" = "123456",
-      "name" = "メルカリ　太朗"
-      "email" = "aaa@gmail.com"
-    })
-  end
+#   def google_mock
+#     Omniauth.config.mock_auth[:google]
+#       "provider" = "google",
+#       "uid" = "123456",
+#       "name" = "メルカリ　太朗"
+#       "email" = "aaa@gmail.com"
+#     })
+#   end
 
-  def facebook_invalid_mock
-    OmniAuth.config.mock_auth[:facebook] = :invalid_credentails
-  end
-end
+#   def facebook_invalid_mock
+#     OmniAuth.config.mock_auth[:facebook] = :invalid_credentails
+#   end
+# end
