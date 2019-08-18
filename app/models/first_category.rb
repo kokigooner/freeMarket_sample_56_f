0419ffccf0ac1a7525210dd66a393ef5f7b4559c
katsu18 <52535077+katsu18@users.noreply.github.com>
2019-08-18 19:36:59 +0900

    reset

--- app/models/first_category.rb
+++ app/models/first_category.rb
@@ -1,5 +1,4 @@
 class FirstCategory < ApplicationRecord
   has_many :brand_categories 
   has_many :brands, through: :brand_categories 
-  has_many :products
 end
--- app/models/product.rb
+++ app/models/product.rb
@@ -1,6 +1,5 @@
 class Product < ApplicationRecord
   has_many :images,dependent: :destroy
   belongs_to :user
-  belongs_to :first_category
 end
 
