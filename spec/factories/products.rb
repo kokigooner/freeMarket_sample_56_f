b4c23185bc99b4c33d19277834ea0621eaa27fd4
katsu18 <52535077+katsu18@users.noreply.github.com>
2019-08-18 19:33:10 +0900

    Update products.rb

--- spec/factories/products.rb
+++ spec/factories/products.rb
@@ -3,7 +3,7 @@ FactoryBot.define do
     user_id{'1'}
     product_name{'テストプロダクト'}
     price{'test@exa'}
-    description{'testtest'}
+    description{'testtest'} 
     first_category_id{'1'}
     second_category_id{'1'}
     third_category_id{'1'}
