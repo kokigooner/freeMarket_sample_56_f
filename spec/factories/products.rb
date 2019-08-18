ddaec8757ba5328cda7433f692541fb3d5853814
katsu18 <52535077+katsu18@users.noreply.github.com>
2019-08-18 19:25:06 +0900

    Update products.rb

--- spec/factories/products.rb
+++ spec/factories/products.rb
@@ -4,17 +4,17 @@ FactoryBot.define do
     product_name { 'テストプロダクト' }
     price { 'test@exa' } 
     description { 'testtest' }
-    # first_category { "1" }
-    # second_category_id { '1' }
-    # third_category_id { '1' }
-    # brand_id { '1' }
-    # size_id { 'M' }
-    # condition { 'size_id' }
+    first_category { "1" }
+    second_category_id { '1' }
+    third_category_id { '1' }
+    brand_id { '1' }
+    size_id { 'M' }
+    condition { 'size_id' }
     delivery_charge{"送料込み(出品者負担)"}
     delivery_date{"1~2日で発送"}
     delivery_way{"らくらくメルカリ便"}
-    # order_id{"1"}
-    # profit_id{"1"}
+    order_id{"1"}
+    profit_id{"1"}
     order_status{"1"}
   end
 end
