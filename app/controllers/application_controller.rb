9308ed5f63381b1a746788c37ed92a9af2b9f8ee
katsu18 <52535077+katsu18@users.noreply.github.com>
2019-08-20 13:39:49 +0900

    修正

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -17,14 +17,9 @@ class ApplicationController < ActionController::Base
 
   def search_set
   @s = params[:q]
-    if @s != nil
-      params[:q]['product_name_cont_all'] = params[:q]['product_name_cont_all'].split(/[\p{blank}\s]+/)
-      @q = Product.ransack(params[:q])
-      @s_products = @q.result
-    else
-      @q = Product.ransack(params[:q])
-      @s_products = @q.result
-    end
+  params[:q]['product_name_cont_all'] = params[:q]['product_name_cont_all'].split(/[\p{blank}\s]+/) unless @s = nil
+  @q = Product.ransack(params[:q])
+  @s_products = @q.result
   end
 
 
