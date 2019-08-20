d56be30f35d812f0670ee4d4b6ce1198fb7d5920
yuichi.kitagawa <nagi09041192@yahoo.co.jp>
2019-08-20 20:50:38 +0900

    change-seed.rb

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,3 +1,367 @@
+<<<<<<< Updated upstream
+=======
+FirstCategory.create!(
+  [
+    {
+      first_category: 'レディース',
+    },
+    {
+      first_category: 'メンズ',
+    },
+    {
+      first_category: 'ベビー・キッズ',
+    },
+    {
+      first_category: 'インテリア・住まい・小物',
+    },
+    {
+      first_category: '本・音楽・ゲーム',
+    },
+    {
+      first_category: 'おもちゃ・ホビー・グッズ',
+    },
+    {
+      first_category: 'コスメ・香水・美容',
+    },
+    {
+      first_category: '家電・スマホ・カメラ',
+    },
+    {
+      first_category: 'スポーツ・レジャー',
+    },
+    {
+      first_category: 'ハンドメイド',
+    },
+    {
+      first_category: 'チケット',
+    },
+    {
+      first_category: '自動車・オートバイ',
+    },
+    {
+      first_category: 'その他',
+    }
+  ]
+)
+
+SecondCategory.create!(
+  [
+    {
+      first_category_id: '1',
+      second_category: 'トップス',
+    },
+    {
+      first_category_id: '1',
+      second_category: 'ジャケット/アウター',
+    },
+    {
+      first_category_id: '1',
+      second_category: 'パンツ',
+    },
+    {
+      first_category_id: '1',
+      second_category: 'スカート',
+    },
+    {
+      first_category_id: '1',
+      second_category: 'ワンピース',
+    },
+    {
+      first_category_id: '1',
+      second_category: 'その他',
+    },
+    {
+      first_category_id: '2',
+      second_category: 'トップス',
+    },
+    {
+      first_category_id: '2',
+      second_category: 'ジャケット/アウター',
+    },
+    {
+      first_category_id: '2',
+      second_category: 'パンツ',
+    },
+    {
+      first_category_id: '2',
+      second_category: '靴',
+    },
+    {
+      first_category_id: '2',
+      second_category: 'バッグ',
+    },
+    {
+      first_category_id: '2',
+      second_category: 'その他',
+    },
+    {
+      first_category_id: '3',
+      second_category: 'ベビー服(女の子用)~95cm',
+    },
+    {
+      first_category_id: '3',
+      second_category: 'ベビー服(男の子用)~95cm',
+    },
+    {
+      first_category_id: '3',
+      second_category: 'ベビー服(男女兼用)~95cm',
+    },
+    {
+      first_category_id: '3',
+      second_category: 'キッズ服(女の子用)100cm~',
+    },
+    {
+      first_category_id: '3',
+      second_category: 'キッズ服(男の子用)100cm〜',
+    },
+    {
+      first_category_id: '3',
+      second_category: 'その他',
+    },
+    {
+      first_category_id: '4',
+      second_category: 'キッチン/食器',
+    },
+    {
+      first_category_id: '4',
+      second_category: 'ベッド/マットレス',
+    },
+    {
+      first_category_id: '4',
+      second_category: 'ソファ/ソファベッド',
+    },
+    {
+      first_category_id: '4',
+      second_category: '椅子/チェア',
+    },
+    {
+      first_category_id: '4',
+      second_category: '机/テーブル',
+    },
+    {
+      first_category_id: '4',
+      second_category: 'その他',
+    },
+    {
+      first_category_id: '5',
+      second_category: '本',
+    },
+    {
+      first_category_id: '5',
+      second_category: '漫画',
+    },
+    {
+      first_category_id: '5',
+      second_category: '雑誌',
+    },
+    {
+      first_category_id: '5',
+      second_category: 'CD',
+    },
+    {
+      first_category_id: '5',
+      second_category: 'DVD/ブルーレイ',
+    },
+    {
+      first_category_id: '5',
+      second_category: 'その他',
+    },
+    {
+      first_category_id: '6',
+      second_category: 'おもちゃ',
+    },
+    {
+      first_category_id: '6',
+      second_category: 'タレントグッズ',
+    },
+    {
+      first_category_id: '6',
+      second_category: 'コミック/アニメグッズ',
+    },
+    {
+      first_category_id: '6',
+      second_category: 'トレーディングカード',
+    },
+    {
+      first_category_id: '6',
+      second_category: 'フィギュア',
+    },
+    {
+      first_category_id: '6',
+      second_category: 'その他',
+    },
+    {
+      first_category_id: '7',
+      second_category: 'ベースメイク',
+    },
+    {
+      first_category_id: '7',
+      second_category: 'メイクアップ',
+    },
+    {
+      first_category_id: '7',
+      second_category: 'ネイルケア',
+    },
+    {
+      first_category_id: '7',
+      second_category: '香水',
+    },
+    {
+      first_category_id: '7',
+      second_category: 'スキンケア/基礎化粧品',
+    },
+    {
+      first_category_id: '7',
+      second_category: 'その他',
+    },
+    {
+      first_category_id: '8',
+      second_category: 'スマートフォン/携帯電話',
+    },
+    {
+      first_category_id: '8',
+      second_category: 'スマホアクセサリー',
+    },
+    {
+      first_category_id: '8',
+      second_category: 'PC/タブレット',
+    },
+    {
+      first_category_id: '8',
+      second_category: 'カメラ',
+    },
+    {
+      first_category_id: '8',
+      second_category: 'テレビ/映像機器',
+    },
+    {
+      first_category_id: '8',
+      second_category: 'その他',
+    },
+    {
+      first_category_id: '9',
+      second_category: 'ゴルフ',
+    },
+    {
+      first_category_id: '9',
+      second_category: 'フィッシング',
+    },
+    {
+      first_category_id: '9',
+      second_category: '自転車',
+    },
+    {
+      first_category_id: '9',
+      second_category: 'トレーニング/エクササイズ',
+    },
+    {
+      first_category_id: '9',
+      second_category: '野球',
+    },
+    {
+      first_category_id: '9',
+      second_category: 'その他',
+    },
+    {
+      first_category_id: '10',
+      second_category: 'アクセサリー(女性用)',
+    },
+    {
+      first_category_id: '10',
+      second_category: 'ファッション/小物',
+    },
+    {
+      first_category_id: '10',
+      second_category: 'アクセサリー/時計',
+    },
+    {
+      first_category_id: '10',
+      second_category: '日用品/インテリア',
+    },
+    {
+      first_category_id: '10',
+      second_category: '趣味/おもちゃ',
+    },
+    {
+      first_category_id: '10',
+      second_category: 'その他',
+    },
+    {
+      first_category_id: '11',
+      second_category: '音楽',
+    },
+    {
+      first_category_id: '11',
+      second_category: 'スポーツ',
+    },
+    {
+      first_category_id: '11',
+      second_category: '演劇/芸能',
+    },
+    {
+      first_category_id: '11',
+      second_category: 'イベント',
+    },
+    {
+      first_category_id: '11',
+      second_category: '映画',
+    },
+    {
+      first_category_id: '11',
+      second_category: 'その他',
+    },
+    {
+      first_category_id: '12',
+      second_category: '自動車本体',
+    },
+    {
+      first_category_id: '12',
+      second_category: '自動車本体/ホイール',
+    },
+    {
+      first_category_id: '12',
+      second_category: '自動車パーツ',
+    },
+    {
+      first_category_id: '12',
+      second_category: '自動車アクセサリー',
+    },
+    {
+      first_category_id: '12',
+      second_category: 'オートバイ本体',
+    },
+    {
+      first_category_id: '12',
+      second_category: 'その他',
+    },
+    {
+      first_category_id: '13',
+      second_category: 'まとめ売り',
+    },
+    {
+      first_category_id: '13',
+      second_category: 'ペット用品',
+    },
+    {
+      first_category_id: '13',
+      second_category: '食品',
+    },
+    {
+      first_category_id: '13',
+      second_category: '飲料/酒',
+    },
+    {
+      first_category_id: '13',
+      second_category: '日用品/生活雑貨/旅行',
+    },
+    {
+      first_category_id: '13',
+      second_category: 'その他',
+    },
+  ]
+)
+
+>>>>>>> Stashed changes
 ThirdCategory.create!(
   [
     {
@@ -1525,4 +1889,39 @@ ThirdCategory.create!(
       third_category: 'その他',
     },
   ]
+<<<<<<< Updated upstream
 )
+=======
+)
+
+
+# 以下はカテゴリ検索の実装用の商品データです
+# (1..381).each do |n|
+#   third_category = ThirdCategory.find(n)
+#   second_category_id = ThirdCategory.find(n)[:second_category_id]
+#   second_category = SecondCategory.find(second_category_id)
+#   first_category_id = SecondCategory.find(second_category_id)[:first_category_id]
+#   first_category = FirstCategory.find(first_category_id)
+
+#   Product.create!(
+#     first_category_id: first_category_id,
+#     second_category_id: second_category_id,
+#     third_category_id: n,
+#     user_id: 1,
+#     product_name: "大カテゴリ#{first_category.first_category}:中カテゴリ#{second_category.second_category}:小カテゴリ#{third_category.third_category}の商品",
+#     price: 3000,
+#     description: "サンプル商品です",
+#     condition: "傷や汚れあり",
+#     delivery_charge: "送料込み(出品者負担)",
+#     delivery_date: "1~2日で発送",
+#     delivery_way: "未定"
+#   )
+# end
+
+# (1..381).each do |n|
+#   Image.create!(
+#     product_id: n,
+#     image: File.open("./app/assets/images/mercarisampleitem4.jpg")
+#   )
+# end
+>>>>>>> Stashed changes
