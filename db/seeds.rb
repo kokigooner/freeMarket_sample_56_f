99f8a18007f3c636d4a0d7e4504c1900b042ab9f
kaseken <k.kaseda0925@gmail.com>
2019-08-20 10:01:34 +0900

    add_category_data_to_database

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,46 +1,46 @@
-FirstCategory.create!(
-  [
-    {
-      first_category: 'レディース',
-    },
-    {
-      first_category: 'メンズ',
-    },
-    {
-      first_category: 'ベビー・キッズ',
-    },
-    {
-      first_category: 'インテリア・住まい・小物',
-    },
-    {
-      first_category: '本・音楽・ゲーム',
-    },
-    {
-      first_category: 'おもちゃ・ホビー・グッズ',
-    },
-    {
-      first_category: 'コスメ・香水・美容',
-    },
-    {
-      first_category: '家電・スマホ・カメラ',
-    },
-    {
-      first_category: 'スポーツ・レジャー',
-    },
-    {
-      first_category: 'ハンドメイド',
-    },
-    {
-      first_category: 'チケット',
-    },
-    {
-      first_category: '自動車・オートバイ',
-    },
-    {
-      first_category: 'その他',
-    }
-  ]
-)
+# FirstCategory.create!(
+#   [
+#     {
+#       first_category: 'レディース',
+#     },
+#     {
+#       first_category: 'メンズ',
+#     },
+#     {
+#       first_category: 'ベビー・キッズ',
+#     },
+#     {
+#       first_category: 'インテリア・住まい・小物',
+#     },
+#     {
+#       first_category: '本・音楽・ゲーム',
+#     },
+#     {
+#       first_category: 'おもちゃ・ホビー・グッズ',
+#     },
+#     {
+#       first_category: 'コスメ・香水・美容',
+#     },
+#     {
+#       first_category: '家電・スマホ・カメラ',
+#     },
+#     {
+#       first_category: 'スポーツ・レジャー',
+#     },
+#     {
+#       first_category: 'ハンドメイド',
+#     },
+#     {
+#       first_category: 'チケット',
+#     },
+#     {
+#       first_category: '自動車・オートバイ',
+#     },
+#     {
+#       first_category: 'その他',
+#     }
+#   ]
+# )
 
 SecondCategory.create!(
   [
