055a1b33bf7040bd639db1efa312790e43d5e298
kaseken <k.kaseda0925@gmail.com>
2019-08-21 17:41:51 +0900

    fix_view_for_purchase_confirmation

--- config/routes.rb
+++ config/routes.rb
@@ -7,7 +7,6 @@ Rails.application.routes.draw do
   match 'secondcategory', to: 'products#secondcategory', via: [:get, :post]
   match 'thirdcategory', to: 'products#thirdcategory', via: [:get, :post]
   get '/products/search', to: 'products#search'
-  get "/products/confirm", to: "products#confirm"
   get "/products/sell", to: "products#sell"
 
   resources :products, only: [:index, :show, :create] do
